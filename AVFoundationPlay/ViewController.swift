93f8bd413d24a4bec72098f839baa4909f05eca3
Gene De Lisa <gene@rockhoppertech.com>
2016-01-08 10:18:41 -0500

    Initial Commit

---
+++ AVFoundationPlay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AVFoundationPlay
+//
+//  Created by Gene De Lisa on 1/8/16.
+//  Copyright © 2016 Gene De Lisa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
